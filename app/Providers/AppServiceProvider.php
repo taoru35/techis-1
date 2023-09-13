d71844888f54013366bcb0008e23cb901d2ba9e9
taoru35 <128706444+taoru35@users.noreply.github.com>
2023-09-13 16:44:44 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
     }
 
     /**
@@ -23,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
+
+        // 本番環境でHTTPSを強制
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+
     }
 }
