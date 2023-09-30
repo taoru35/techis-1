fa76a84071fc03f32eb57a95ecb0eead4d424650
taoru35 <128706444+taoru35@users.noreply.github.com>
2023-09-30 12:30:42 +0900

    stripe追加

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -5,71 +5,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>チェックアウト</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Stripe.jsライブラリを読み込み -->
+    <script src="https://js.stripe.com/v3/"></script>
 </head>
 <body>
 
-<!-- Navbar -->
 @include('partials.navbar')
 
 <div class="container mt-5">
     <h2 class="my-4">購入商品確認画面</h2>
 
     @if(!is_null($cartItems) && count($cartItems) > 0)
-        <h4 class="mb-3">カートの中身</h4>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>アイテム名</th>
-                    <th>価格(税抜き)</th>
-                    <th>数量</th>
-                    <th>合計(税抜き)</th>
-                </tr>
-            </thead>
-            <tbody>
-                @php
-                $grandTotal = 0;
-                @endphp
-                @foreach($cartItems as $item)
-                    @php
-                        $taxExcludedPrice = $item->price;
-                        $totalTaxExcluded = $taxExcludedPrice * (isset($item->pivot) ? $item->pivot->quantity : 0);
-                        $grandTotal += $totalTaxExcluded;
-                    @endphp
-                    <tr>
-                        <td>{{ $item->name }}</td>
-                        <td>
-                            {{ number_format($taxExcludedPrice * 1.10, 0) }}円
-                            ({{ number_format($taxExcludedPrice, 0) }}円)
-                        </td>
-                        <td>{{ isset($item->pivot) ? $item->pivot->quantity : '' }}</td>
-                        <td>
-                            {{ number_format($totalTaxExcluded * 1.10, 0) }}円
-                            ({{ number_format($totalTaxExcluded, 0) }}円)
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <!-- ... 既存のカート表示コード ... -->
 
-        <div class="mt-4">
-            <h4>全商品の合計: <span style="color: red; font-weight: bold; background-color: #ffe5e5; padding: 3px;">{{ number_format($grandTotal * 1.10, 0) }}円</span> (税込)</h4>
-
-        </div>
-
-        <!-- ここに決済手段の選択や住所の入力など、チェックアウトに関連するフォーム要素を追加できます。 -->
-
-        <form action="{{ route('checkout.store') }}" method="post" class="mt-3">
+        <form action="{{ route('checkout.store') }}" method="post" id="payment-form" class="mt-3">
             @csrf
+
+            <!-- クレジットカード情報の入力フォーム -->
+            <div class="form-group">
+                <label for="card-element">
+                    クレジットカード情報
+                </label>
+                <div id="card-element"></div>
+                <div id="card-errors" role="alert" class="text-danger mt-2"></div>
+            </div>
+
             <button type="submit" class="btn btn-primary" style="background-color: #2491f0f8;">購入する</button>
         </form>
 
     @else
         <p>カートにアイテムがありません。</p>
     @endif
-
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script>
+    var stripe = Stripe('pk_test_51NqoaULML00WK7RX3QKujDunU7N3QpMlpJtFeVmZtCHE6mght7knZk4RnErzBA0Ac3uVgTyZI7UCMHcVWrszMtkd00QIiulrPl');
+    var elements = stripe.elements();
+    var card = elements.create('card');
+    card.mount('#card-element');
+
+    var form = document.getElementById('payment-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        stripe.createToken(card).then(function(result) {
+            if (result.error) {
+                var errorElement = document.getElementById('card-errors');
+                errorElement.textContent = result.error.message;
+            } else {
+                var hiddenInput = document.createElement('input');
+                hiddenInput.setAttribute('type', 'hidden');
+                hiddenInput.setAttribute('name', 'stripeToken');
+                hiddenInput.setAttribute('value', result.token.id);
+                form.appendChild(hiddenInput);
+                form.submit();
+            }
+        });
+    });
+</script>
+
 </body>
 </html>
