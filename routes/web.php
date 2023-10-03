1da5f08a1d6cc5611ebb849e8d8469f1f3f8214a
taoru35 <128706444+taoru35@users.noreply.github.com>
2023-10-03 17:52:10 +0900

    ルート修正

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalonController;
@@ -7,56 +6,69 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
-
+use App\Http\Controllers\NotificationController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 Auth::routes();
-
+// ユーザー、管理者、スタッフ全てがアクセス可能なページ
 Route::get('/salon', [SalonController::class, 'homepage'])->name('salon.homepage');
-
+// 管理者とスタッフのみがアクセス可能なページ
 Route::middleware(['ensureRoleIsAdminOrStaff'])->group(function () {
-
+    // ダッシュボードページ
     Route::get('/', [HomeController::class, 'index'])->name('home');
-
+    // 商品管理ページ
     Route::prefix('items')->group(function () {
         Route::get('/', [ItemController::class, 'index'])->name('items.index');
-        Route::get('/create', [ItemController::class, 'create'])->name('items.create');
-        Route::post('/', [ItemController::class, 'store'])->name('items.store');
-        Route::get('/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
-        Route::put('/{id}', [ItemController::class, 'update'])->name('items.update');
+        Route::get('/add', [ItemController::class, 'add'])->name('items.addForm');
+        Route::post('/add', [ItemController::class, 'add'])->name('items.add');
+        Route::get('/edit/{id}', [ItemController::class, 'edit'])->name('items.edit');
+        Route::put('/edit/{id}', [ItemController::class, 'update'])->name('items.update');
         Route::delete('/{id}', [ItemController::class, 'destroy'])->name('items.destroy');
+        // ->middleware(['auth']); // ミドルウェアを適用する場合はこれを使用
     });
+    // ユーザー関連ページ
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('users/store', [UserController::class, 'store'])->name('users.store');
+});
+// カスタマー関連ページ
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
+Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
 
-    Route::resource('users', UserController::class)->except(['edit', 'update', 'show']);
+Route::post('/customers/store', [CustomerController::class, 'store'])->name('customers.store');
 
-    Route::prefix('notifications')->group(function () {
-        Route::get('/', [NotificationController::class, 'index'])->name('notifications.index');
-        Route::get('/create', [NotificationController::class, 'create'])->name('notifications.create');
-        Route::post('/', [NotificationController::class, 'store'])->name('notifications.store');
-        Route::get('/{notification}/edit', [NotificationController::class, 'edit'])->name('notifications.edit');
-        Route::put('/{notification}', [NotificationController::class, 'update'])->name('notifications.update');
-        Route::delete('/{notification}', [NotificationController::class, 'destroy'])->name('notifications.destroy');
-    });
+// 通知関連ページ
+Route::prefix('notifications')->middleware(['ensureRoleIsAdminOrStaff'])->group(function () {
+    Route::get('/', [NotificationController::class, 'index'])->name('notifications.index'); // 通知の一覧
+    Route::get('/create', [NotificationController::class, 'create'])->name('notifications.create'); // 通知の作成ページ
+    Route::post('/store', [NotificationController::class, 'store'])->name('notifications.store'); // 通知の保存
 
+    // 必要に応じて、他のルート（例：edit、update、deleteなど）も追加できます。
 });
 
-Route::resource('customers', CustomerController::class)->except(['edit', 'update', 'show']);
 
 // ショップ関連
 Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
 Route::get('shop/{item}', [ShopController::class, 'show'])->name('shop.show');
-
-// カート関連
-Route::prefix('cart')->group(function () {
-    Route::get('/', [CartController::class, 'index'])->name('cart.index');
-    Route::post('/add/{item}', [CartController::class, 'add'])->name('cart.add');
-    Route::delete('/remove/{item}', [CartController::class, 'remove'])->name('cart.remove');
-    Route::post('/increase/{item}', [CartController::class, 'increaseQuantity'])->name('cart.increaseQuantity');
-    Route::post('/decrease/{item}', [CartController::class, 'decreaseQuantity'])->name('cart.decreaseQuantity');
-});
-
-// 決済関連
+// カート
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/add/{item}', [CartController::class, 'add'])->name('cart.add');
+Route::delete('/cart/remove/{item}', [CartController::class, 'remove'])->name('cart.remove');
+Route::delete('/cart/{item}', [CartController::class, 'remove'])->name('cart.remove');
+Route::post('/cart/increase/{item}', [CartController::class, 'increaseQuantity'])->name('cart.increaseQuantity');
+Route::post('/cart/decrease/{item}', [CartController::class, 'decreaseQuantity'])->name('cart.decreaseQuantity');
+// 決済画面
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
 Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
